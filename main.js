ca86d76a9a9845951c227a99b58220d994d1eb85
ECOFRIENDLYFIRE <kolesoadam@gmail.com>
2025-02-09 20:01:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCybLXpHSdFLPlPBxecMpmzStuSxOIDJ6Y",
+  authDomain: "sbor-7a773.firebaseapp.com",
+  databaseURL: "https://sbor-7a773-default-rtdb.firebaseio.com",
+  projectId: "sbor-7a773",
+  storageBucket: "sbor-7a773.firebasestorage.app",
+  messagingSenderId: "382666971780",
+  appId: "1:382666971780:web:e82286a6212319c60dc0d1",
+  measurementId: "G-B7K9KSSS4P"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref("messages");
+
+document.getElementById("send").addEventListener("click", () => {
+    let message = document.getElementById("message").value;
+    if (message) {
+        db.push().set({ text: message });
+        document.getElementById("message").value = "";
+    }
+});
+
+db.on("child_added", snapshot => {
+    let msg = snapshot.val().text;
+    let p = document.createElement("p");
+    p.textContent = msg;
+    document.getElementById("chat-box").appendChild(p);
+});
